988d7935b021ad022f1d2a6c18815ef7bcc791e4
Robin Cai <robinrainup@gmail.com>
2025-09-17 10:01:57 -0400

    Update a1_assignment_xinyu_robin_cai.js

--- a1_assignment_xinyu_robin_cai/a1_assignment_xinyu_robin_cai.js
+++ a1_assignment_xinyu_robin_cai/a1_assignment_xinyu_robin_cai.js
@@ -24,23 +24,63 @@ let hospital = {
 
 
 
+// function showPatients(hospitalData){
+//     let result = `<h1>${hospitalData.name}</h1>\n`;
+
+//     for (const patient of hospitalData.patientRecords) {
+
+//         let patientData = 
+//         `<h2>${patient.name}, ${patient.dateOfBirth} </h2>\n  <ul>\n`;
+
+//         for (const symptom of patient.symptoms) {
+//             patientData += `    <li>${symptom}</li>\n`;
+//         }
+
+//         patientData += `  </ul>\n`;
+//         result += patientData;
+//     }
+//     return result;
+// }
+
+
+function createEle(element, content){
+    let newElement = document.createElement(element);
+    newElement.textContent = content;
+    return newElement;
+}
+
+
+
+function createList(list){
+    let newUl = document.createElement("ul");
+
+    for(const e of list){
+        let newLi = document.createElement("li");
+        newLi.textContent = e;
+        newUl.appendChild(newLi);
+    }
+
+    return newUl;
+}
+
+
+
 function showPatients(hospitalData){
-    let result = `<h1>${hospitalData.name}</h1>\n`;
+    let root = createEle("div", "")
+    let hospitalName = createEle("h1", hospitalData.name);
+    root.appendChild(hospitalName);
 
     for (const patient of hospitalData.patientRecords) {
 
-        let patientData = 
-        `<h2>${patient.name}, ${patient.dateOfBirth} </h2>\n  <ul>\n`;
+        let patientName = createEle("h2", `${patient.name}, ${patient.dateOfBirth}`);
+        let patientSymp = createList(patient.symptoms);
+        root.appendChild(patientName);
+        root.appendChild(patientSymp);
 
-        for (const symptom of patient.symptoms) {
-            patientData += `    <li>${symptom}</li>\n`;
-        }
-
-        patientData += `  </ul>\n`;
-        result += patientData;
     }
-    return result;
+    return root;
 }
+
 
 
 
